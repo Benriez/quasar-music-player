57aa0aa07b9eb6d4659fd82682bd39c0846c93d0
Benriez <stberiez@stud.h-da.de>
2020-08-15 12:46:41 +0200

    structure the basic layout

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,29 @@
 <template>
   <q-page class="flex flex-center">
+     <q-card class="my-card" flat>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
 
+        <q-item-section>
+          <q-item-label>Ananas Express</q-item-label>
+          <q-item-label caption>
+            Audiomatic
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+
+      <q-card-actions align="around">
+        <q-btn flat round size="2.715em" color="red" icon="skip_previous" />
+        <q-btn flat round size="2.715em" color="teal" icon="play_arrow" />
+        <q-btn flat round size="2.715em" color="primary" icon="skip_next" />
+      </q-card-actions>
+      </q-card>
   </q-page>
 </template>
 
@@ -9,3 +32,6 @@ export default {
   name: 'PageIndex'
 }
 </script>
+
+<style lang="stylus">
+</style>
